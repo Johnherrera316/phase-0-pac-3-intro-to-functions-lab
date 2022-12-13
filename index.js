e4020df02fac59289f0db39251d9cc4740edf8ca
Jonathan Herrera <jonathanherrera2356@gmail.com>
2022-12-13 11:03:31 -0600

    commit tree fe12b09dd4aa7adf8a1449d4741e4cfd5d625249

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+    //todo
+}
+function shout(string) {
+    return string;
+}
+function shout(string) {
+    return string.toUpperCase();
+}function whisper(string) {
+    //todo
+}
+function whisper(string) {
+    return string;
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    //todo
+}
+function logShout(string) {
+    console.log('HELLO')
+}
+function logWhisper(string) {
+    //todo
+}
+function logWhisper(string) {
+    console.log('hello')
+}
+function sayHiToHeadphonedRoommate(string) {
+    //todo
+}
+function sayHhToHeadphonedRoomate(string) {
+    console.log('I can/'t hear you!')
+}
